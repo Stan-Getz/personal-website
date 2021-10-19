9c805c22933f1eb124d75e29b85b40a20e03a1c0
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-20 00:03:16 +0200

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Header/Navbar';
+import Hero from './components/Hero/Hero';
+import backgroundImg from './banner.jpg';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar />
+        <div
+          className="background-img"
+          style={{
+            backgroundImage: `linear-gradient(to bottom, rgba(8, 8, 8, 1), rgba(28, 28, 28, 0.7), rgba(195, 195, 195, 0.25)), url(${backgroundImg})`,
+          }}
+        >
+          <Navbar />
+          <Hero />
+        </div>
+        <div className="footer">
+          <h1>Footer</h1>
+        </div>
       </React.Fragment>
     );
   }
