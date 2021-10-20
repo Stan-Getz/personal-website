e323a8c5c9d0b4d463b6a63898e17f20c05d57a5
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-20 12:08:41 +0200

    Fixed display issues with background img

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Header/Navbar';
 import Hero from './components/Hero/Hero';
-import backgroundImg from './banner.jpg';
 
 class App extends React.Component {
   render() {
@@ -11,7 +10,8 @@ class App extends React.Component {
         <div
           className="background-img"
           style={{
-            backgroundImage: `linear-gradient(to bottom, rgba(8, 8, 8, 1), rgba(28, 28, 28, 0.7), rgba(195, 195, 195, 0.25)), url(${backgroundImg})`,
+            backgroundImage:
+              'linear-gradient(to bottom, rgba(8, 8, 8, 1), rgba(28, 28, 28, 0.7), rgba(195, 195, 195, 0.25)), url(/img/banner.jpg)',
           }}
         >
           <Navbar />
