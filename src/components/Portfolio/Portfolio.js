c94ce619155702754706cb602955c8a2af18096b
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-21 02:43:08 +0200

    Added portfolio component and its style

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Portfolio.css';
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <div className="pf-container">
+        <div className="pf-title">
+          <h2>my portfolio</h2>
+        </div>
+        <p>to be added soon...</p>
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
