41564c9853b298c400baa591d63b496baba93bcb
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-21 02:45:06 +0200

    Added description to hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './Hero.css';
 
 class Hero extends React.Component {
   render() {
     return (
-      <div className="hero-container">
-        <div className="hero-content">
-          <h1 className="hero-heading">
-            Web Developer <br />
-            <span className="cs-hide-on-mobile">& Coffee Enthusiast</span>
-          </h1>
-
-          <div className="hero-btn-container">
-            <button className="portfolio-btn">Check out my projects</button>
+      <Router>
+        <div className="hero-container">
+          <div className="hero-content">
+            <h1>
+              Web Developer
+              <span className="cs-hide-on-mobile">
+                <br />& Coffee Enthusiast
+              </span>
+            </h1>
+            <div className="text-container">
+              <p>
+                I'm passionate about software and technologies and consider that
+                everyone should learn to code in order to get the most out of
+                the things that surround us and just because coding means a lot
+                of fun. As my next career step, I'd love to be a part of a
+                company which brings state-of-the-art technologies in web
+                development to more and more customers, and thus to contribute
+                with my own experience and newly gained knowledge to making the
+                world a more user-friendly place.
+              </p>
+            </div>
+            <div className="hero-btn-container">
+              <Link to="portfolio">
+                <button className="portfolio-btn">Check out my projects</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
