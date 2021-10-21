105580c19dc1ec6ceffa5de3dce74fff6667ee6f
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-21 02:43:27 +0200

    Added about component and its style

---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './About.css';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className="about-container">
+        <div className="about-title">
+          <h2>about me</h2>
+        </div>
+        <p>to be added soon...</p>
+      </div>
+    );
+  }
+}
+
+export default About;
