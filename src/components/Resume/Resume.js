1e8991a71fdf528ffd6544318bf3b62be634cb64
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-21 02:43:45 +0200

    Added resume component and its style

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Resume.css';
+
+class Resume extends React.Component {
+  render() {
+    return (
+      <div className="resume-container">
+        <div className="resume-title">
+          <h2>my resume</h2>
+        </div>
+        <p>to be added soon...</p>
+      </div>
+    );
+  }
+}
+
+export default Resume;
