3a2687386c5dfb34c6de1faa174b67e4ba38523d
Stan-Getz <stanislav.genunchi@gmail.com>
2021-10-21 02:46:38 +0200

    Added fontawesome icons to library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+  faEnvelopeSquare,
+  faAngleDoubleDown,
+} from '@fortawesome/free-solid-svg-icons';
+
 import './index.css';
 import App from './App';
+
+library.add(fab, faEnvelopeSquare, faAngleDoubleDown);
 
 ReactDOM.render(
   <React.StrictMode>
